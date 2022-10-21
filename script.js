93ba6584a53d360ac038c09301da2c29363eca9f
atanko123 <anze.8tanko@gmail.com>
2022-10-21 17:39:29 +0200

    Add eyes

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("hello")
+const wrapper = document.getElementById("wrapper")
+const wrapperPosition = wrapper.getBoundingClientRect()
+
+const wrapperWidth = wrapperPosition.width
+const wrapperHeight = wrapperPosition.height
+
+const eyeWrapper = document.getElementsByClassName("eyes")
+const eyeWrapperWidth = eyeWrapper[0].getBoundingClientRect().width
+
+const eyes = document.getElementsByClassName("eye")
+const eyeWidth = eyes[0].getBoundingClientRect().width
+
+addEventListener('mousemove', (e) => {
+    const mouseX = e.clientX
+    const mouseY = e.clientY
+
+    const position = calculateEyePosition(mouseX, mouseY)
+
+    for (let eye of eyes) {
+        eye.style.left = position.x + "px"
+        eye.style.top = position.y + "px"
+    }
+})
+
+function calculateEyePosition (mouseX, mouseY) {
+    return {
+        x: calculatePosition(mouseX, wrapperWidth),
+        y: calculatePosition(mouseY, wrapperHeight),
+    }
+}
+
+function calculatePosition (mousePosition, wrapperSize) {
+    const position = mousePosition / wrapperSize * eyeWrapperWidth - eyeWidth
+    return position < 0 ? 0 : position
+}
